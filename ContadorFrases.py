5590b64f5d27ff850ca1a9f5e46bb148d978634a
c.soler13 <109521>
2022-05-30 10:58:30 +0200

    Test Frase aleatoria OK

--- ContadorFrases.py
+++ ContadorFrases.py
@@ -1,15 +1,25 @@
+import random
+
+
 class ContadorFrases:
 
     def calculador(self, peticion, fichero):
-        fichero_abierto = open(fichero, "r")
-        f = fichero_abierto.read()
-        lineas = f.count("\n") +1
-        #lineas = str(len(fichero_abierto.readlines()))
+        if peticion.upper() == 'TOTAL':
+            fichero_abierto = open(fichero, "r")
+            f = fichero_abierto.read()
+            lineas = f.count("\n") +1
+            #lineas = str(len(fichero_abierto.readlines()))
 
-        Counter = 0
-        CoList = f.split("\n")
-        for i in CoList:
-            if i:
-                Counter += 1
+            Counter = 0
+            CoList = f.split("\n")
+            for i in CoList:
+                if i:
+                    Counter += 1
+            return '%d FRASES %d LINEAS' % (Counter, lineas)
 
-        return '%d FRASES %d LINEAS' % (Counter, lineas)
+        elif peticion.upper() == 'FRASE':
+            fichero_abierto = open(fichero, "r")
+            f = fichero_abierto.read()
+            CoList = f.split("\n")
+
+            return random.choice(CoList)
