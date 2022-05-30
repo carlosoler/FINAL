a86c1b3dd827b274090a53535c23f963738b79f6
c.soler13 <109521>
2022-05-30 11:07:16 +0200

    Refactor calculador eliminacion de codigo duplicado

--- ContadorFrases.py
+++ ContadorFrases.py
@@ -4,19 +4,16 @@ import random
 class ContadorFrases:
 
     def calculador(self, peticion, fichero):
+        fichero_abierto = open(fichero, "r")
+        f = fichero_abierto.read()
+        ficheroSinSaltoLinea = f.split("\n")
         if peticion.upper() == 'TOTAL':
-            fichero_abierto = open(fichero, "r")
-            f = fichero_abierto.read()
-            lineas = f.count("\n") +1
+            lineas = f.count("\n") + 1
             contador = 0
-            ficheroSinSaltoLinea = f.split("\n")
             for frase in ficheroSinSaltoLinea:
                 if frase:
                     contador += 1
             return '%d FRASES %d LINEAS' % (contador, lineas)
 
         elif peticion.upper() == 'FRASE':
-            fichero_abierto = open(fichero, "r")
-            f = fichero_abierto.read()
-            ficheroSinSaltoLinea = f.split("\n")
             return random.choice(ficheroSinSaltoLinea)
