ccad67556fc9b0f2dfac483ba4a00fb815fa7833
c.soler13 <109521>
2022-05-30 12:28:59 +0200

    SERVIDOR OK y CLIENTE pero sin pasar 3 mensajes

--- cliente.py
+++ cliente.py
@@ -7,14 +7,16 @@ addres_serv = ('0.0.0.0', 16023)
 
 peticion = input("Que desea?: FRASE o TOTAL: ")
 
+#array_Peticiones = ["FRASE", "TOTAL", "tonto"]
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 0))
 
 try:
     s.connect(addres_serv)
-    s.send(peticion.encode('utf-8'))
+    s.sendto(peticion.encode('utf-8'), addres_serv)
 
-    recibido = s.recv(1000).decode('utf-8').strip()
+    recibido = s.recv(10000).decode('utf-8').strip()
     print(recibido)
     s.close()
 except IOError as e:
--- servidor.py
+++ servidor.py
@@ -4,13 +4,14 @@ from ContadorFrases\
 import errno
 import os.path
 
-fichero = 'DATOS'
+fichero = 'ejemplo_passwd'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 s.bind(('0.0.0.0', 16023))
 s.listen(1)
+
 
 while True:
     ns, dir_cli = s.accept()
@@ -20,7 +21,7 @@ while True:
         resultado = cont.calculador(peticion, fichero)
 
         ns.send(resultado.encode('utf-8'))
-        ns.close()
+
     except IOError as e:
         if e.errno == errno.EPIPE:
             print("ERROR")
