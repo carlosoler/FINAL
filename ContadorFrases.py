429776444cefb377c97419d2d66cafc0dee1901c
c.soler13 <109521>
2022-05-30 11:24:07 +0200

    Test pasarle un fichero inexistente OK

--- ContadorFrases.py
+++ ContadorFrases.py
@@ -1,22 +1,25 @@
+import os
 import random
 
 
 class ContadorFrases:
 
     def calculador(self, peticion, fichero):
-        fichero_abierto = open(fichero, "r")
-        f = fichero_abierto.read()
-        ficheroSinSaltoLinea = f.split("\n")
-        if peticion.upper() == 'TOTAL':
-            lineas = f.count("\n") + 1
-            contador = 0
-            for frase in ficheroSinSaltoLinea:
-                if frase:
-                    contador += 1
-            return '%d FRASES %d LINEAS' % (contador, lineas)
+        if os.path.exists(fichero):
+            fichero_abierto = open(fichero, "r")
+            f = fichero_abierto.read()
+            ficheroSinSaltoLinea = f.split("\n")
+            if peticion.upper() == 'TOTAL':
+                lineas = f.count("\n") + 1
+                contador = 0
+                for frase in ficheroSinSaltoLinea:
+                    if frase:
+                        contador += 1
+                return '%d FRASES %d LINEAS' % (contador, lineas)
 
-        elif peticion.upper() == 'FRASE':
-            return random.choice(ficheroSinSaltoLinea)
-
+            elif peticion.upper() == 'FRASE':
+                return random.choice(ficheroSinSaltoLinea)
+            else:
+                return "ERROR"
         else:
             return "ERROR"
