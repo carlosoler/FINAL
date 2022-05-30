196429bbbbbfc0bd9ea44666237ec2a695259d35
c.soler13 <109521>
2022-05-30 10:01:26 +0200

    Test devolver total

---
+++ testContadorFrases.py
@@ -0,0 +1,15 @@
+import unittest
+from ContadorFrases import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_ContarTotal(self):
+        peticion = 'TOTAL'
+        fichero = 'ficheroPrueba'
+        cont = ContadorFrases()
+        resultado = cont.calculador(peticion, fichero)
+        esperado = "3 FRASES 1 LINEAS"
+
+
+if __name__ == '__main__':
+    unittest.main()
